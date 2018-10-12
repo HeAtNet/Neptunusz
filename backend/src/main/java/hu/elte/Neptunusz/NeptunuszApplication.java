67ac1676998528907884d8e268a9ddea7a362b69
Attila Herczog <herczog.at97@gmail.com>
2018-10-12 17:52:14 +0200

    Add generated Spring project

---
+++ backend/src/main/java/hu/elte/Neptunusz/NeptunuszApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.Neptunusz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeptunuszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeptunuszApplication.class, args);
+	}
+}
