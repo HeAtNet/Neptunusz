088b446ad86e634b8c0af689c0956bfbc4eef9f1
gavrillas <gavrillaskristof@gmail.com>
2018-11-04 01:28:04 +0100

    SQL query added and subject controller created

---
+++ Neptunusz/src/main/java/hu/elte/Neptunusz/controllers/SubjectController.java
@@ -0,0 +1,73 @@
+package hu.elte.Neptunusz.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hu.elte.Neptunusz.entities.Subject;
+import hu.elte.Neptunusz.repositories.SubjectRepository;
+
+
+@RestController
+@RequestMapping("/subject")
+public class SubjectController {
+	
+	@Autowired
+	private SubjectRepository subjectRepository;
+	
+	@GetMapping("")
+    public ResponseEntity<Iterable<Subject>> getAll() {
+        return ResponseEntity.ok(subjectRepository.findAll());
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Subject> get(@PathVariable Integer id) {
+        Optional<Subject> subject = subjectRepository.findById(id);
+        if (subject.isPresent()) {
+            return ResponseEntity.ok(subject.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+	
+	@PostMapping("")
+    public ResponseEntity<Subject> post(@RequestBody Subject subject) {
+        Subject savedSubject = subjectRepository.save(subject);
+        return ResponseEntity.ok(savedSubject);
+    }
+	
+	 @PutMapping("/{id}")
+	 public ResponseEntity<Subject> update
+	         (@PathVariable Integer id,
+	           @RequestBody Subject subject) {
+	        Optional<Subject> oSubject = subjectRepository.findById(id);
+	        if (oSubject.isPresent()) {
+	            subject.setId(id);
+	            return ResponseEntity.ok(subjectRepository.save(subject));
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+	 
+	 @DeleteMapping("/{id}")
+	    public ResponseEntity<Subject> delete
+	            (@PathVariable Integer id) {
+	        Optional<Subject> oSubject= subjectRepository.findById(id);
+	        if (oSubject.isPresent()) {
+	            subjectRepository.deleteById(id);
+	            return ResponseEntity.ok().build();
+	        } else {
+	            return ResponseEntity.notFound().build();
+	        }
+	    }
+
+}
--- Neptunusz/src/main/resources/data.sql
+++ Neptunusz/src/main/resources/data.sql
@@ -1,3 +1,9 @@
-insert into user (id, username, name, mail, pass, start_date, status, role, is_del) values (1, 'ASD123', 'name0', 'mail0', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', CURRENT_TIMESTAMP(), 'ACTIVE', 'STUDENT', false);
-insert into user (id, username, name, mail, pass, start_date, status, role, is_del) values (2, 'SDF234', 'name1', 'mail1', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', CURRENT_TIMESTAMP(), 'ACTIVE', 'STUDENT', false);
-insert into user (id, username, name, mail, pass, start_date, status, role, is_del) values (3, 'DFG345', 'name2', 'mail2', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', CURRENT_TIMESTAMP(), 'ACTIVE', 'STUDENT', false);
+insert into user (id, username, name, mail, pass, start_date, status, role, is_del) values (1, 'ASD123', 'name0', 'mail0', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', CURRENT_TIMESTAMP(), 'ACTIVE', 'ROLE_STUDENT', false);
+insert into user (id, username, name, mail, pass, start_date, status, role, is_del) values (2, 'SDF234', 'name1', 'mail1', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', CURRENT_TIMESTAMP(), 'ACTIVE', 'ROLE_STUDENT', false);
+insert into user (id, username, name, mail, pass, start_date, status, role, is_del) values (3, 'DFG345', 'name2', 'mail2', '$2a$04$YDiv9c./ytEGZQopFfExoOgGlJL6/o0er0K.hiGb5TGKHUL8Ebn..', CURRENT_TIMESTAMP(), 'ACTIVE', 'ROLE_STUDENT', false);
+
+insert into subject (name,start_of_lesson,end_of_lesson,classroom,type,is_del) values ('Analízis','16:15','17:45',821,'PRACTICE',false);
+
+insert into exam (start_of_exam,end_of_exam,classroom,type,is_del, subject_id) values ('8:00','11:00','0-822- Bólyai', 'ORAL',false,1);
+
+insert into message (title,text,sender_id,adresse_id) values ('Óra elmaradás','Kedves hallgatók! A mai gyakorlat elmarad',1,2);
