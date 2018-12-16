0f9956e49e763d012b0e00ad7a7bec0c7e90aa48
gavrillas <gavrillaskristof@gmail.com>
2018-12-16 19:13:00 +0100

    CrossOrigin annotation added

--- Neptunusz/src/main/java/hu/elte/Neptunusz/controllers/MessageController.java
+++ Neptunusz/src/main/java/hu/elte/Neptunusz/controllers/MessageController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import hu.elte.Neptunusz.entities.Message;
 import hu.elte.Neptunusz.repositories.MessageRepository;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
